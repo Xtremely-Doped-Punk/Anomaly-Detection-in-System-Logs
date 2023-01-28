5842019f37117edf5bcbb0a67efc3c59b80d8b29
Logesh <vlogesh2001@gmail.com>
2023-01-28 17:00:25 +0530

    logbert

---
+++ Dependencies/bert_pytorch/dataset/sample.py
@@ -0,0 +1,159 @@
+from tqdm import tqdm
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def generate_pairs(line, window_size):
+    line = np.array(line)
+    line = line[:, 0]
+
+    seqs = []
+    for i in range(0, len(line), window_size):
+        seq = line[i:i + window_size]
+        seqs.append(seq)
+    seqs += []
+    seq_pairs = []
+    for i in range(1, len(seqs)):
+        seq_pairs.append([seqs[i - 1], seqs[i]])
+    return seqs
+
+
+def fixed_window(line, window_size, adaptive_window, seq_len=None, min_len=0, debug=False):
+    if debug:
+        print("- -"*20)
+        print("Fixed Window on line:",line)
+
+    line = [ln.split(",") for ln in line.split()] # 'key,time' mapping split
+    if debug:
+        print("splited line:",line)
+        print("window_size:",window_size,"adaptive_window:",adaptive_window)
+        print("seq_len:",seq_len,"min_len:",min_len)
+
+    # filter the line/session shorter than 10
+    if len(line) < min_len:
+        if debug:
+            print("len of line is lesser than min_len...\n\
+            ...returning empty logkey_seqs & time_seqs")
+        return [], []
+
+    # max seq len
+    if seq_len is not None:
+        if debug:
+            print("truncating line till seq_len")
+        line = line[:seq_len]
+
+    if adaptive_window:
+        window_size = len(line)
+        if debug:
+            print("adaptive window size override:",window_size)
+
+    line = np.array(line)
+    if debug:
+        print("line shape as np.array:",line.shape)
+
+    # if time duration exists in data
+    if line.shape[1] == 2:
+        tim = line[:,1].astype(float)
+        line = line[:, 0]
+        if debug:
+            print("Time Duration exists in Data")
+            print("time:",tim,"\tline:",line)
+
+        # the first time duration of a session should be 0, so max is window_size(mins) * 60
+        tim[0] = 0
+    else:
+        line = line.squeeze()
+        if (line.shape==()):
+            line = np.array([line])
+        # if time duration doesn't exist, then create a zero array for time
+        tim = np.zeros(line.shape)
+        
+        if debug:
+            print("Time Duration doesn't exists in Data (empty arr created)")
+            print("time:",tim,"\tline:",line)
+
+    logkey_seqs = []
+    time_seq = []
+    for i in range(0, len(line), window_size):
+        logkey_seqs.append(line[i:i + window_size])
+        time_seq.append(tim[i:i + window_size])
+
+    return logkey_seqs, time_seq
+
+
+def generate_train_valid(data_path, window_size=20, adaptive_window=True,
+                         sample_ratio=1, valid_size=0.1, output_path=None,
+                         scale=None, scale_path=None, seq_len=None, min_len=0,debug=False):
+    with open(data_path, 'r') as f:
+        data_iter = f.readlines()
+    
+    num_session = int(len(data_iter) * sample_ratio)
+    # only even number of samples, or drop_last=True in DataLoader API
+    # coz in parallel computing in CUDA, odd number of samples reports issue when merging the result
+    # num_session += num_session % 2
+
+    test_size = int(min(num_session, len(data_iter)) * valid_size)
+    # only even number of samples
+    # test_size += test_size % 2
+
+    valid_size = round(test_size/num_session,3)
+    # update split size
+
+    print("before filtering short session")
+    print("train size ", int(num_session - test_size))
+    print("valid size ", int(test_size))
+    
+    if debug:
+        print()
+        print("= ="*20)
+        print("Generate-Train-Valid on data_path:", data_path,"\n>>> Data:")
+        print(data_iter)
+
+    logkey_seq_pairs = []
+    time_seq_pairs = []
+    session = 0
+    for line in tqdm(data_iter):
+        if session >= num_session:
+            break
+        session += 1
+        if debug:
+            print("\t session:",session)
+        logkeys, times = fixed_window(line, window_size, adaptive_window, seq_len, min_len, debug=debug)
+        if debug:
+            print()
+            print("logkeys:",logkeys)
+            print("times:",times)
+        logkey_seq_pairs += logkeys
+        time_seq_pairs += times
+
+    logkey_seq_pairs = np.array(logkey_seq_pairs)
+    time_seq_pairs = np.array(time_seq_pairs)
+    if debug:
+        print('- -'*20)
+        print('logkey_seq_pairs:',logkey_seq_pairs)
+        print("time_seq_pairs:",time_seq_pairs)
+
+    logkey_trainset, logkey_validset, time_trainset, time_validset = train_test_split(logkey_seq_pairs,
+                                                                                      time_seq_pairs,
+                                                                                      test_size=valid_size,
+                                                                                      random_state=1234)
+
+    # sort seq_pairs by seq len
+    train_len = list(map(len, logkey_trainset))
+    valid_len = list(map(len, logkey_validset))
+
+    train_sort_index = np.argsort(-1 * np.array(train_len))
+    valid_sort_index = np.argsort(-1 * np.array(valid_len))
+
+    logkey_trainset = logkey_trainset[train_sort_index]
+    logkey_validset = logkey_validset[valid_sort_index]
+
+    time_trainset = time_trainset[train_sort_index]
+    time_validset = time_validset[valid_sort_index]
+
+    print("="*40)
+    print("Num of train seqs", len(logkey_trainset))
+    print("Num of valid seqs", len(logkey_validset))
+    print("="*40)
+
+    return logkey_trainset, logkey_validset, time_trainset, time_validset
---
+++ Dependencies/bert_pytorch/dataset/vocab.py
@@ -0,0 +1,192 @@
+import pickle
+import tqdm
+from collections import Counter
+import sys
+sys.path.append("../")
+
+class TorchVocab(object):
+    """Defines a vocabulary object that will be used to numericalize a field.
+    Attributes:
+        freqs: A collections.Counter object holding the frequencies of tokens
+            in the data used to build the Vocab.
+        stoi: A collections.defaultdict instance mapping token strings to
+            numerical identifiers.
+        itos: A list of token strings indexed by their numerical identifiers.
+    """
+
+    def __init__(self, counter, max_size=None, min_freq=1, specials=['<pad>', '<oov>'],
+                 vectors=None, unk_init=None, vectors_cache=None, debug=False):
+        """Create a Vocab object from a collections.Counter.
+        Arguments:
+            counter: collections.Counter object holding the frequencies of
+                each value found in the data.
+            max_size: The maximum size of the vocabulary, or None for no
+                maximum. Default: None.
+            min_freq: The minimum frequency needed to include a token in the
+                vocabulary. Values less than 1 will be set to 1. Default: 1.
+            specials: The list of special tokens (e.g., padding or eos) that
+                will be prepended to the vocabulary in addition to an <unk>
+                token. Default: ['<pad>']
+            vectors: One of either the available pretrained vectors
+                or custom pretrained vectors (see Vocab.load_vectors);
+                or a list of aforementioned vectors
+            unk_init (callback): by default, initialize out-of-vocabulary word vectors
+                to zero vectors; can be any function that takes in a Tensor and
+                returns a Tensor of the same size. Default: torch.Tensor.zero_
+            vectors_cache: directory for cached vectors. Default: '.vector_cache'
+        """
+        self.debug = debug
+        self.freqs = counter
+        if(self.debug):
+            print("Word-Count-Freq's:",self.freqs)
+
+        counter = counter.copy()
+        min_freq = max(min_freq, 1) # min_freq cant be less than 1, i.e., atleast once the word should have occured in the text
+
+        self.itos = list(specials)
+        if(self.debug):
+            print("Special Tokens:",self.itos)
+
+        # frequencies of special tokens are not counted when building vocabulary
+        # in frequency order
+        for tok in specials:
+            del counter[tok]
+        if(self.debug):
+            print("Counter after removing freqs of special tokens:",counter)
+
+        max_size = None if max_size is None else max_size + len(self.itos)
+
+        # sort by frequency, then alphabetically
+        words_and_frequencies = sorted(counter.items(), key=lambda tup: tup[0])
+        if(self.debug):
+            print("Sorted by Freq",words_and_frequencies)
+        words_and_frequencies.sort(key=lambda tup: tup[1], reverse=True)
+        
+        if(self.debug):
+            print("Sorted by Freq, then alphabetically",words_and_frequencies)
+            print("min_freq constraint:",min_freq)
+            print("max no. of special tokens:",max_size)
+
+        for word, freq in words_and_frequencies: # iterate 'word' and 'freq' at the same time
+            if freq < min_freq or len(self.itos) == max_size:
+                break # we are stopping when either
+                # the freq of word is less than min_freq (or) no.of special tokens in list has reached max_limit
+                # as we have already sorted the words wrt their freq's first itself
+            self.itos.append(word)
+        
+        # stoi is simply a reverse dict for itos
+        self.stoi = {tok: i for i, tok in enumerate(self.itos)}
+        
+        if(self.debug):
+            print("Final List of Special Tokens:",self.itos)
+            print("Switching Keys and Values:",self.stoi)
+
+        self.vectors = None
+        if vectors is not None:
+            self.load_vectors(vectors, unk_init=unk_init, cache=vectors_cache)
+        else:
+            assert unk_init is None and vectors_cache is None
+
+    def __eq__(self, other):
+        if self.freqs != other.freqs:
+            return False
+        if self.stoi != other.stoi:
+            return False
+        if self.itos != other.itos:
+            return False
+        if self.vectors != other.vectors:
+            return False
+        return True
+
+    def __len__(self):
+        return len(self.itos)
+
+    def vocab_rerank(self):
+        self.stoi = {word: i for i, word in enumerate(self.itos)}
+
+    def extend(self, v, sort=False):
+        words = sorted(v.itos) if sort else v.itos
+        for w in words:
+            if w not in self.stoi:
+                self.itos.append(w)
+                self.stoi[w] = len(self.itos) - 1
+
+
+class Vocab(TorchVocab):
+    def __init__(self, counter, max_size=None, min_freq=1, debug=False):
+        self.pad_index = 0
+        self.unk_index = 1
+        self.eos_index = 2
+        self.sos_index = 3
+        self.mask_index = 4
+        super().__init__(counter, specials=["<pad>", "<unk>", "<eos>", "<sos>", "<mask>"],
+                         max_size=max_size, min_freq=min_freq, debug=debug)
+
+    def to_seq(self, sentece, seq_len, with_eos=False, with_sos=False) -> list:
+        pass
+
+    def from_seq(self, seq, join=False, with_pad=False):
+        pass
+
+    @staticmethod
+    def load_vocab(vocab_path: str) -> 'Vocab':
+        with open(vocab_path, "rb") as f:
+            return pickle.load(f)
+
+    def save_vocab(self, vocab_path):
+        with open(vocab_path, "wb") as f:
+            pickle.dump(self, f)
+
+
+# Building Vocab with text files
+class WordVocab(Vocab):
+    def __init__(self, texts, max_size=None, min_freq=1, debug=False):
+        print("Building Vocab")
+        counter = Counter()
+        for line in tqdm.tqdm(texts):
+            if isinstance(line, list):
+                words = line
+            else:
+                words = line.replace("\n", "").replace("\t", "").split()
+        
+            for word in words:
+                counter[word] += 1
+        
+        print()    
+        super().__init__(counter, max_size=max_size, min_freq=min_freq,debug=debug)
+
+    def to_seq(self, sentence, seq_len=None, with_eos=False, with_sos=False, with_len=False):
+        if isinstance(sentence, str):
+            sentence = sentence.split()
+
+        seq = [self.stoi.get(word, self.unk_index) for word in sentence]
+
+        if with_eos:
+            seq += [self.eos_index]  # this would be index 1
+        if with_sos:
+            seq = [self.sos_index] + seq
+
+        origin_seq_len = len(seq)
+
+        if seq_len is None:
+            pass
+        elif len(seq) <= seq_len:
+            seq += [self.pad_index for _ in range(seq_len - len(seq))]
+        else:
+            seq = seq[:seq_len]
+
+        return (seq, origin_seq_len) if with_len else seq
+
+    def from_seq(self, seq, join=False, with_pad=False):
+        words = [self.itos[idx]
+                 if idx < len(self.itos)
+                 else "<%d>" % idx
+                 for idx in seq
+                 if not with_pad or idx != self.pad_index]
+
+        return " ".join(words) if join else words
+
+    @staticmethod
+    def load_vocab(vocab_path: str) -> 'WordVocab':
+        with open(vocab_path, "rb") as f:
+            return pickle.load(f)

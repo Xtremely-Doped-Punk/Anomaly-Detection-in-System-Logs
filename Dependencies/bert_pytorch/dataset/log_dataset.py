daae1a74897155800223bf7cae9e37d2c2aaf619
Logesh <vlogesh2001@gmail.com>
2023-01-30 16:07:08 +0530

    Update log_dataset.py

--- Dependencies/bert_pytorch/dataset/log_dataset.py
+++ Dependencies/bert_pytorch/dataset/log_dataset.py
@@ -38,8 +38,7 @@ class LogDataset(IterableDataset):
         self.debug = debug
 
     def get_data(self):
-        if !debug: return
-    else return {'log_corpus':log_corpus,'time_corpus':time_corpus}
+        return {'log_corpus':log_corpus,'time_corpus':time_corpus}
 
     def __len__(self):
         return self.corpus_lines
